52703f97bf25db825f0197a2293e9692ed7e3f1a
pratu880 <abhishekjagtap149@gmail.com>
2025-09-30 23:32:24 +0100

    update print

--- task2.py
+++ task2.py
@@ -2,6 +2,6 @@ Original_list = [1,2,3,4,5,6,7,8,9,10]
 # print(Original_list)
 extract_list = Original_list[:5]
 print(f"Original list: {Original_list}")
-print(f"Extracted fist five elements: {extract_list}")
+print(f"Extracted first five elements: {extract_list}")
 extract_list.reverse()
 print(f"Reversed extracted elements: {extract_list}")
